b5581a29c8e97bb620fe89502fb8919b3f71e260
Houssam <hossameissmaal13@gmail.com>
2026-03-24 19:56:00 +0100

    fix railway port

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
+app.get("/", (req, res) => {
+  res.send("BTS Network API is running 🚀");
+});
 
 app.use('/api/students', studentRoutes);
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () =>
-  console.log(`🚀 SERVER RUNNING ON PORT ${PORT}`)
-);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.error("❌ MongoDB connection error:", err));
+
+const PORT = 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 SERVER RUNNING ON PORT ${PORT}`);
+});
